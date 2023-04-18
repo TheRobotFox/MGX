28817b6b29ad24bd2b43a8cbe5727448f7841d8c
Tomke <pfoch.tomke@gmail.com>
2023-04-18 02:49:45 +0200

    PTR

--- MGX_type.h
+++ MGX_type.h
@@ -4,17 +4,24 @@
 #define __MGX_T_CAT_(a, ...) a ## __VA_ARGS__
 #define __MGX_T_CAT2(a,b) __MGX_T_CAT2_(a,b)
 #define __MGX_T_CAT2_(a,b) a ## b
+#define __MGX_T_EXPAND(...) __VA_ARGS__
 
-
+#define __MGX_T_IS_COMP_restrict true
 #define __MGX_T_IS_COMP_struct true
 #define __MGX_T_IS_COMP_union true
+#define __MGX_T_IS_COMP_enum true
+#define __MGX_T_IS_COMP_const true
+#define __MGX_T_IS_COMP_static true
+#define __MGX_T_IS_COMP_volatile true
+
 #define __MGX_T_IS_ARRAY_ARRAY(...) true
+#define __MGX_T_IS_PTR_PTR(...) true
 
 #define __MGX_T_IS_ARRAY(T) __MGX_T_CAT_(__MGX_T_IS_ARRAY_, T)
+#define __MGX_T_IS_PTR(T) __MGX_T_CAT_(__MGX_T_IS_PTR_, T)
 #define __MGX_T_IS_COMP(T) __MGX_T_CAT_(__MGX_T_IS_COMP_, T)
 
 #define __MGX_T_ARRAY_FORWARD(M, T, ...) M __MGX_LPAREN __MGX_T_CAT_(__MGX_T_EXTRACT_, T) ,  __VA_ARGS__ __MGX_RPAREN
-#define __MGX_T_EXTRACT_ARRAY(T, S) T, S
 #define __MGX_T_FORWARD_RECURSIVE_INDIRECT() __MGX_T_FORWARD_RECURSIVE_
 #define __MGX_T_FORWARD_RECURSIVE_CALL(FUNCTION, ARG, M, L, ...) __MGX_DEFER_n(L, FUNCTION)\
 											(__MGX_DEFER2(__MGX_T_FORWARD_RECURSIVE_INDIRECT)() (M, MGX_DEC(MGX_DEC(L)), __VA_ARGS__), ARG)
@@ -24,10 +31,13 @@
 
 
 #define __MGX_T_FORWARD_RECURSIVE_(M, L, ...) MGX_IF(__MGX_T_IS_ARRAY(MGX_ARG_FIRST(__VA_ARGS__)))\
+				   (__MGX_T_FORWARD_RECURSIVE_ARRAY(M, L, __VA_ARGS__), MGX_IF(__MGX_T_IS_PTR(MGX_ARG_FIRST(__VA_ARGS__))) \
 				   (__MGX_T_FORWARD_RECURSIVE_ARRAY(M, L, __VA_ARGS__), MGX_IF(__MGX_T_IS_COMP(MGX_ARG_FIRST(__VA_ARGS__))) \
-														      (M ## _COMP(__VA_ARGS__), M(__VA_ARGS__)))
+														      (M ## _COMP(__VA_ARGS__), M(__VA_ARGS__))))
+
 #define __MGX_T_FORWARD_RECURSIVE(M, ...) __MGX_EVAL(__MGX_T_FORWARD_RECURSIVE_(M, __MGX_DEFER_MAX, __VA_ARGS__))
 
+#define __MGX_T_WHILE(c, ...) __MGX_T_EVAL(__MGX_WHILE(c, __VA_ARGS__))
 
 #define __MGX_T_GET_NAME_COMP(T) __MGX_T_COMP_EXTRACT(__MGX_T_COMP_GLUE, T)
 #define __MGX_T_COMP_EXTRACT(M, T) __MGX_EXPAND(M __MGX_LPAREN __MGX_T_CAT_(__MGX_T_COMP_NAME_, T) __MGX_RPAREN)
@@ -42,11 +52,30 @@
 #define __MGX_T_GET_VAR(T, name) T name
 #define __MGX_T_GET_VAR_COMP(T, name) T name
 #define __MGX_T_GET_VAR_ARRAY(A, S) A [S]
+#define __MGX_T_GET_VAR_PTR(A) (*A)
 
 
 // Functions
 #define MGX_T_NAME(T) __MGX_T_FORWARD_RECURSIVE(__MGX_T_GET_NAME, T)
-#define MGX_T_VAR(T, name) __MGX_T_FORWARD_RECURSIVE(__MGX_T_GET_VAR, T, name)
+//#define MGX_T_VAR(T, name) __MGX_T_FORWARD_RECURSIVE(__MGX_T_GET_VAR, T, name)
 #define MGX_T_VAR_PTR(T, name) MGX_T_VAR(T, __MGX_DEFER_n(__MGX_DEFER_MAX, __MGX_LPAREN_)() __MGX_DEFER_n(__MGX_DEFER_MAX, __MGX_STAR_)()name __MGX_DEFER_n(__MGX_DEFER_MAX, __MGX_RPAREN_)())
 #define MGX_T_PTR(T) MGX_IF(__MGX_T_IS_ARRAY(T))(MGX_T_VAR_PTR(T, ), T*)
 #define MGX_T(T) MGX_T_VAR(T,)
+
+#define __MGX_T_NOT_COMP(...) MGX_NOT(__MGX_T_CAT_(__MGX_T_IS_COMP(MGX_ARG_FIRST(__VA_ARGS__))))
+#define __MGX_T_FORWARD(M, R, T) M, __MGX_T_EXPAND(__MGX_T_CAT(M##_, __MGX_T_CAT_(__MGX_T_POSTFIX, T)) __MGX_T_FORWARD_ARGLIST(R,T) )
+//#define __MGX_T_FORWARD_(P, M, R, T) __MGX_T_EXPAND(M##_##P __MGX_T_FORWARD_ARGLIST_(R,T) )
+
+#define __MGX_T_FORWARD_ARGLIST(R,T) ( R, __MGX_CAT_(__MGX_T_EXTRACT_, T) )
+
+#define __MGX_T_EXTRACT_ARRAY(T, S) T, S
+#define __MGX_T_EXTRACT_PTR(T) T
+
+#define __MGX_T_POSTFIX_PTR(...) PTR
+#define __MGX_T_POSTFIX_ARRAY(...) ARRAY
+
+#define MGX_T_VAR(T, name) __MGX_T_WHILE(__MGX_T_NOT_COMP, __MGX_T_FORWARD, __MGX_T_GET_VAR, name, T )
+
+MGX_T(ARRAY(extern static volatile restrict const struct abc*, 3))
+MGX_T(PTR(ARRAY(extern static volatile restrict const struct abc*, 3)))
+MGX_T(struct abc)
