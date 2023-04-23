0e98bf487d36dcf1a5f5cfe7a4b3555d37d39b21
Tomke <pfoch.tomke@gmail.com>
2023-04-23 16:48:08 +0200

    Fast multiplication

--- MGX_type.h
+++ MGX_type.h
@@ -18,6 +18,77 @@
 #define __MGX_T_EVAL4(...) __MGX_T_EVAL5(__MGX_T_EVAL5(__MGX_T_EVAL5(__VA_ARGS__)))
 #define __MGX_T_EVAL5(...) __VA_ARGS__
 
+#define __MGX_T_inv_3_5_Table_100 0
+#define __MGX_T_inv_3_5_Table_97 5
+#define __MGX_T_inv_3_5_Table_94 10
+#define __MGX_T_inv_3_5_Table_91 15
+#define __MGX_T_inv_3_5_Table_88 20
+#define __MGX_T_inv_3_5_Table_85 25
+#define __MGX_T_inv_3_5_Table_82 30
+#define __MGX_T_inv_3_5_Table_79 35
+#define __MGX_T_inv_3_5_Table_76 40
+#define __MGX_T_inv_3_5_Table_73 45
+#define __MGX_T_inv_3_5_Table_70 50
+#define __MGX_T_inv_3_5_Table_67 55
+#define __MGX_T_inv_3_5_Table_64 60
+#define __MGX_T_inv_3_5_Table_61 65
+#define __MGX_T_inv_3_5_Table_58 70
+#define __MGX_T_inv_3_5_Table_55 75
+#define __MGX_T_inv_3_5_Table_52 80
+#define __MGX_T_inv_3_5_Table_49 85
+#define __MGX_T_inv_3_5_Table_46 90
+#define __MGX_T_inv_3_5_Table_43 95
+#define __MGX_T_inv_3_5_Table_40 100
+#define __MGX_T_inv_3_5_Table_37 105
+#define __MGX_T_inv_3_5_Table_34 110
+#define __MGX_T_inv_3_5_Table_31 115
+#define __MGX_T_inv_3_5_Table_28 120
+#define __MGX_T_inv_3_5_Table_25 125
+#define __MGX_T_inv_3_5_Table_22 130
+#define __MGX_T_inv_3_5_Table_19 135
+#define __MGX_T_inv_3_5_Table_16 140
+#define __MGX_T_inv_3_5_Table_13 145
+#define __MGX_T_inv_3_5_Table_10 150
+#define __MGX_T_inv_3_5_Table_7 155
+#define __MGX_T_inv_3_5_Table_4 160
+#define __MGX_T_inv_3_5_Table_1 165
+
+#define __MGX_T_inv_3_4_Table_100 0
+#define __MGX_T_inv_3_4_Table_97 4
+#define __MGX_T_inv_3_4_Table_94 8
+#define __MGX_T_inv_3_4_Table_91 12
+#define __MGX_T_inv_3_4_Table_88 16
+#define __MGX_T_inv_3_4_Table_85 20
+#define __MGX_T_inv_3_4_Table_82 24
+#define __MGX_T_inv_3_4_Table_79 28
+#define __MGX_T_inv_3_4_Table_76 32
+#define __MGX_T_inv_3_4_Table_73 36
+#define __MGX_T_inv_3_4_Table_70 40
+#define __MGX_T_inv_3_4_Table_67 44
+#define __MGX_T_inv_3_4_Table_64 48
+#define __MGX_T_inv_3_4_Table_61 52
+#define __MGX_T_inv_3_4_Table_58 56
+#define __MGX_T_inv_3_4_Table_55 60
+#define __MGX_T_inv_3_4_Table_52 64
+#define __MGX_T_inv_3_4_Table_49 68
+#define __MGX_T_inv_3_4_Table_46 72
+#define __MGX_T_inv_3_4_Table_43 76
+#define __MGX_T_inv_3_4_Table_40 80
+#define __MGX_T_inv_3_4_Table_37 84
+#define __MGX_T_inv_3_4_Table_34 88
+#define __MGX_T_inv_3_4_Table_31 92
+#define __MGX_T_inv_3_4_Table_28 96
+#define __MGX_T_inv_3_4_Table_25 100
+#define __MGX_T_inv_3_4_Table_22 104
+#define __MGX_T_inv_3_4_Table_19 108
+#define __MGX_T_inv_3_4_Table_16 112
+#define __MGX_T_inv_3_4_Table_13 116
+#define __MGX_T_inv_3_4_Table_10 120
+#define __MGX_T_inv_3_4_Table_7 124
+#define __MGX_T_inv_3_4_Table_4 128
+#define __MGX_T_inv_3_4_Table_1 132
+
+#define __MGX_T_FAST_MUL(Z, N) __MGX_T_inv_3_##Z##_Table_##N
 
 #define __MGX_T_IS_COMP_restrict true
 #define __MGX_T_IS_COMP_struct true
@@ -65,7 +136,7 @@
 #define __MGX_T_FORWARD_RECURSIVE_INDIRECT() __MGX_T_FORWARD_RECURSIVE_
 
 #define __MGX_T_FORWARD_RECURSIVE_CALL(FUNCTION, ARGLIST, M, ...) __MGX_DEFER_n(MGX_ARG_FIRST(__VA_ARGS__), FUNCTION)\
-	(MGX_SUB(__MGX_DEFER_MAX, MGX_ARG_FIRST(__VA_ARGS__)), __MGX_DEFER_n(MGX_DEC(MGX_ARG_FIRST(__VA_ARGS__)), __MGX_T_ARG_FIRST_INDIRECT)()(__MGX_T_FORWARD_RECURSIVE_CALL_(M, MGX_ARG_FIRST(__VA_ARGS__), ARGLIST MGX_ARG_TAIL_OPT(__VA_ARGS__))) __MGX_T_EXPAND2( MGX_ARG_TAIL_OPT ARGLIST ) __MGX_DEFER_n(MGX_DEC(MGX_ARG_FIRST(__VA_ARGS__)), MGX_ARG_TAIL_OPT)(__MGX_T_FORWARD_RECURSIVE_CALL_(M, MGX_ARG_FIRST(__VA_ARGS__), ARGLIST MGX_ARG_TAIL_OPT(__VA_ARGS__))) )
+	(MGX_ARG_FIRST(__VA_ARGS__), __MGX_DEFER_n(MGX_DEC(MGX_ARG_FIRST(__VA_ARGS__)), __MGX_T_ARG_FIRST_INDIRECT)()(__MGX_T_FORWARD_RECURSIVE_CALL_(M, MGX_ARG_FIRST(__VA_ARGS__), ARGLIST MGX_ARG_TAIL_OPT(__VA_ARGS__))) __MGX_T_EXPAND2( MGX_ARG_TAIL_OPT ARGLIST ) __MGX_DEFER_n(MGX_DEC(MGX_ARG_FIRST(__VA_ARGS__)), MGX_ARG_TAIL_OPT)(__MGX_T_FORWARD_RECURSIVE_CALL_(M, MGX_ARG_FIRST(__VA_ARGS__), ARGLIST MGX_ARG_TAIL_OPT(__VA_ARGS__))) )
 
 #define __MGX_T_FORWARD_RECURSIVE_CALL_(M, L, ...) __MGX_DEFER3(__MGX_T_FORWARD_RECURSIVE_INDIRECT)() (M, MGX_DEC(MGX_DEC(MGX_DEC(L))), __MGX_T_EXPAND2( __MGX_DEFER2(__MGX_T_ARG_FIRST2_INDIRECT)() MGX_ARG_FIRST(__VA_ARGS__) ) MGX_ARG_TAIL_OPT(__VA_ARGS__))
 
@@ -94,8 +165,8 @@
 
 #define __MGX_T_LPST() (*
 
-#define __MGX_T_DEFER_END(L, M) __MGX_DEFER66 __MGX_DEFER_max __MGX_DEFER_max __MGX_DEFER_n (MGX_MUL(MGX_DIV(L, 3),4), __MGX_EMPTY)()(__MGX_EMPTY)()(__MGX_EMPTY)()(M)
-#define __MGX_T_DEFER_ENDb(L, M) __MGX_DEFER66 __MGX_DEFER_max __MGX_DEFER_max __MGX_DEFER_n (MGX_MUL(MGX_DIV(L, 3),5), __MGX_EMPTY)()(__MGX_EMPTY)()(__MGX_EMPTY)()(M)
+#define __MGX_T_DEFER_END(L, M) __MGX_DEFER66 __MGX_DEFER_max __MGX_DEFER_max __MGX_DEFER_n (__MGX_T_FAST_MUL(4, L), __MGX_EMPTY)()(__MGX_EMPTY)()(__MGX_EMPTY)()(M)
+#define __MGX_T_DEFER_ENDb(L, M) __MGX_DEFER66 __MGX_DEFER_max __MGX_DEFER_max __MGX_DEFER_n (__MGX_T_FAST_MUL(5, L), __MGX_EMPTY)()(__MGX_EMPTY)()(__MGX_EMPTY)()(M)
 
 #define __MGX_T_GET_VAR(T, N) T, , N , ,
 #define __MGX_T_GET_VAR_COMP(L, T, S, P, N, A, ptr) S T, P, N, A, last
